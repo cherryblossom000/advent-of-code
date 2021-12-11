13dd0fd0256bfee2edfdd3713469d9d581881ac6
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-12-11 21:15:10 +1100

    day 9

---
+++ 9/main.hs
@@ -0,0 +1,92 @@
+#!/usr/bin/env runhaskell
+
+{-# LANGUAGE BlockArguments, ImportQualifiedPost #-}
+{-# OPTIONS_GHC -Wall #-}
+
+import Control.Monad       ((<=<), unless)
+import Control.Monad.State (State, execState, get, modify)
+import Data.Foldable       (traverse_)
+import Data.List           qualified as L
+import Data.Map            qualified as M
+import Data.Map            ((!), (!?))
+import Data.Set            qualified as S
+
+type Point = (Int, Int)
+type Input = M.Map Point Int
+
+enumerate :: [a] -> [(Int, a)]
+enumerate = zip [0..]
+
+parse :: String -> Input
+parse = M.fromList .
+  (   (\(i, xs) -> (\(j, x) -> ((i, j), read [x])) <$> enumerate xs)
+  <=< (enumerate . lines)
+  )
+
+data Direction = U | D | L | R deriving (Eq, Show)
+
+allDirs :: [Direction]
+allDirs = [U, D, L, R]
+
+dirFn :: Point -> Direction -> Point
+dirFn (i, j) U = (i - 1, j)
+dirFn (i, j) D = (i + 1, j)
+dirFn (i, j) L = (i, j - 1)
+dirFn (i, j) R = (i, j + 1)
+
+rotL :: Direction -> Direction
+rotL U = L
+rotL R = U
+rotL D = R
+rotL L = D
+
+rotR :: Direction -> Direction
+rotR U = R
+rotR R = D
+rotR D = L
+rotR L = U
+
+lowPoints :: Input -> [Point]
+lowPoints xs = M.foldMapWithKey
+  (\p x -> [p | all (maybe True (>x) . (xs!?) . dirFn p) allDirs])
+  xs
+
+part1 :: Input -> Int
+part1 xs = sum $ (+1) . (xs!) <$> lowPoints xs
+
+type S = State (S.Set Point) ()
+
+search :: Input -> Point -> Point -> Direction -> S
+search xs p p' d = f $ xs !? p'
+  where
+    f (Just x) | x > xs ! p && x /= 9 = search' xs p' [d, rotL d, rotR d]
+    f _ = pure ()
+
+search' :: Input -> Point -> [Direction] -> S
+search' xs p ds = modify (S.insert p) *> traverse_ f ds
+  where
+    f d = get >>= (`unless` search xs p p' d) . (p' `S.member`)
+      where p' = p `dirFn` d
+
+basinPoints :: Input -> Point -> Int
+basinPoints xs p = S.size $ execState (search' xs p allDirs) S.empty
+
+part2 :: Input -> Int
+part2 xs = product . take 3 . L.sortBy (flip compare) $ basinPoints xs <$> lowPoints xs
+
+main :: IO ()
+main = do
+  input <- readFile "input.txt"
+  let inputs = parse <$> [sample, input]
+  traverse_ (print . part1) inputs
+  putStrLn ""
+  traverse_ (print . part2) inputs
+
+sample :: String
+sample = unlines
+  [ "2199943210"
+  , "3987894921"
+  , "9856789892"
+  , "8767896789"
+  , "9899965678"
+  ]
