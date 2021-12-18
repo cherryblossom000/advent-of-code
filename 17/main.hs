4e47bd1988b9beefc92445f2995c6cf4ad9be8a8
cherryblossom <31467609+cherryblossom000@users.noreply.github.com>
2021-12-19 01:07:00 +1100

    improve day 17 code

--- 17/main.hs
+++ 17/main.hs
@@ -6,7 +6,6 @@
 import Data.Foldable (traverse_)
 import Data.Text     qualified as T
 import Data.Text.IO  qualified as T
-import Data.Maybe    (isJust)
 
 toTuple :: [a] -> (a, a)
 toTuple [a, b] = (a, b)
@@ -30,24 +29,30 @@ inTarget :: Target -> Position -> Bool
 inTarget (tx, ty) (x, y) = f x tx && f y ty
   where f a (l, h) = a >= l && a <= h
 
-launch :: Target -> Velocity -> Maybe [Position]
-launch t@((_, maxX), (minY, _)) = go [] . ((0, 0),)
+launch :: Target -> Velocity -> Bool
+launch t@((_, maxX), (minY, _)) = go . ((0, 0),)
   where
-    go ps s | inTarget t p         = Just ps'
-            | x > maxX || y < minY = Nothing
-            | otherwise            = go ps' s'
-      where
-        s'@(p@(x, y), _) = step s
-        ps' = p : ps
+    go s | inTarget t p         = True
+         | x > maxX || y < minY = False
+         | otherwise            = go s'
+      where s'@(p@(x, y), _) = step s
 
 velocities :: Target -> [Velocity]
 velocities ((_, maxX), (minY, _)) = [(x, y) | x <- [0..maxX], y <- [minY..200]]
 
+{-
+v(t) = u - t
+x(t) = \sum_{i=0}^{t-1} v(i) = t(2u + 1 - t) / 2
+x'(t) = 0 => t = (2u + 1) / 2
+x((2u + 1) / 2) = (2u + 1)^2 / 8
+-}
 part1 :: Target -> Int
-part1 t = maximum $ velocities t >>= maybe [] (map snd) . launch t
+part1 t = maximum
+        $  (\(_, vy) -> (2 * vy + 1) ^ (2 :: Int) `div` 8)
+       <$> filter (launch t) (velocities t)
 
 part2 :: Target -> Int
-part2 t = length . filter isJust $ launch t <$> velocities t
+part2 t = length . filter id $ launch t <$> velocities t
 
 main :: IO ()
 main = do
